a9189f5e2fccbadfeb491cadad35f2b0835dc3ae
Victor Hugo Cretton Rivera <victorcrettonrivera@gmail.com>
2026-05-05 19:11:49 -0300

    Bottom bar

--- app/src/main/java/ar/com/premates/MainActivity.kt
+++ app/src/main/java/ar/com/premates/MainActivity.kt
@@ -1,10 +1,10 @@
 package ar.com.premates
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ar.com.premates.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +15,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        configurarNavegacion()
+    }
+
+    private fun configurarNavegacion() {
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHost.navController
+
+        // Conectar bottom nav con navigation component
+        binding.bottomNav.setupWithNavController(navController)
+
+        // Mostrar/ocultar bottom nav según la pantalla
+        navController.addOnDestinationChangedListener { _, destino, _ ->
+            when (destino.id) {
+                R.id.loginFragment -> ocultarBarra()
+                else -> mostrarBarra()
+            }
+        }
+    }
+
+    private fun mostrarBarra() {
+        binding.bottomNav.visibility = View.VISIBLE
+    }
+
+    private fun ocultarBarra() {
+        binding.bottomNav.visibility = View.GONE
     }
 }
